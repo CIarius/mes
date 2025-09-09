025bc993e84b368406f2425b8dd2c14f36ad6259
Clarius <ramccune@googlemail.com>
2025-09-09 14:13:32 +0100

    Anotate servlets rather than map them in web.xml.

--- src/main/java/com/clarius/mes/DashboardServlet.java
+++ src/main/java/com/clarius/mes/DashboardServlet.java
@@ -3,10 +3,12 @@ package com.clarius.mes;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/dashboard")
 public class DashboardServlet {
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
